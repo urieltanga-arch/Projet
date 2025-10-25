b27532aaaa951d034268a94ce0654ac59304ba42
Roy-Senghor <rhountento@gmail.com>
2025-10-25 23:17:28 +0100

    stats ok mais jeu en cours

--- backend/Mon_miam_miam/app/Http/Middleware/CheckRole.php
+++ backend/Mon_miam_miam/app/Http/Middleware/CheckRole.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Support\Facades\Redirect;
 
 class CheckRole
 {
